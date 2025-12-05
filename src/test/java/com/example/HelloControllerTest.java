ac744114fdef838aa11352448bee89fad7b6fa6c
Daniil <daniilvolk1900@gmail.com>
2025-12-05 23:03:45 +0300

    simple app

---
+++ src/test/java/com/example/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+    
+    @Autowired
+    private TestRestTemplate restTemplate;
+    
+    @Test
+    public void testHelloEndpoint() {
+        String response = restTemplate.getForObject("/", String.class);
+        assertThat(response).contains("Hello");
+    }
+}
