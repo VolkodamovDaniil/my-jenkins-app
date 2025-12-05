ff449b2006a9182533e92715e59d6efdd8292fec
Daniil <daniilvolk1900@gmail.com>
2025-12-05 23:08:35 +0300

    simple app

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,24 +1,12 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
     @GetMapping("/")
-    public String home() {
-        return "Welcome to My Jenkins App!";
-    }
-    
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello, %s! This is Jenkins pipeline demo.", name);
-    }
-    
-    @GetMapping("/health")
-    public String health() {
-        return "{\"status\": \"UP\", \"service\": \"my-jenkins-app\"}";
+    public String hello() {
+        return "Hello from Jenkins Pipeline!";
     }
 }
